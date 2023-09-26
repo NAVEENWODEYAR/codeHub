7874712390b2c7383761af2632a51709a7f72443
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-26 19:21:24 +0530

    created the project;

---
+++ CodeWithJava/src/main/java/com/product/CodeWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWithJavaApplication.class, args);
+	}
+
+}
