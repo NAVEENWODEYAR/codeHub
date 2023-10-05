a38a908fbd161be934811d589073289394ce91ac
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 11:50:46 +0530

    data processing operations like filtering, finding, matching, sorting, mapping etc

---
+++ CodeWithJava/src/main/java/com/stream/StreamIntro.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.stream;
+
+/**Streams can be defined as a sequences of elements from a source which support data processing operations. 
+ * You can treat streams as operations on data
+ * 
+ */
+public class StreamIntro
+{
+	/**
+	 * data processing operations like filtering, finding, matching, sorting, mapping etc are very easy using Stream API,
+	 */
+	public StreamIntro() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
