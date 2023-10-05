502e23b64bdb1ebf4ebfce2fa2563826c3b94b74
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 11:47:29 +0530

    Iterator<Student2> itr = studentList2.iterator();

--- CodeWithJava/src/main/java/com/test/Student2Impl.java
+++ CodeWithJava/src/main/java/com/test/Student2Impl.java
@@ -11,7 +11,17 @@ public class Student2Impl
 					studentList2.add(new Student2(1,"Kevin","Clara",27,"Abudabi"));
 					studentList2.add(new Student2(10,"Jones","Thomas",27,"Berlin"));
 					studentList2.add(new Student2(4,"Madam","Dora",87,"Kolar"));
-					System.out.println(studentList2);
+					studentList2.add(new Student2(5,"Madam","Dora",87,"Kolar"));
+					studentList2.add(new Student2(4,"Mam","Dora",87,"Kolar"));
+					
+					
+					// Iterating the collection,
+					Iterator<Student2> itr = studentList2.iterator();
+					
+					while (itr.hasNext()) 
+					{
+						System.out.println(itr.next());
+					}
 	
 	
 	}
