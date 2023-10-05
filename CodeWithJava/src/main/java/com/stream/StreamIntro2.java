adbbdf54f567686e8607b15f99d43db50d631009
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 12:26:18 +0530

    Laptop(lapId=17, lapName=Air, lapMake=Mac, lapModel=2027.0, lapPrice=148765.0)

--- CodeWithJava/src/main/java/com/stream/StreamIntro2.java
+++ CodeWithJava/src/main/java/com/stream/StreamIntro2.java
@@ -17,10 +17,24 @@ public class StreamIntro2
 	static void createStrm()
 	{
 		Stream<Laptop> lapStream = Stream.of(new Laptop(1,"X1Carbon","Lenovo",2017,198765));
+		System.out.println(lapStream);
 	}
+	
+	// 3. of(T… values) : Creates a stream from values
+		static void createStrms()
+		{
+			Stream<Laptop> lapStream = Stream.of(new Laptop(1,"X1Carbon","Lenovo",2017,198765),
+													new Laptop(7,"Yoga","Lenovo",2019,98765),
+													new Laptop(9,"Vostro","Dell",2011,48765),
+													new Laptop(8,"ThinkPad","Lenovo",2023,98765),
+													new Laptop(17,"Air","Mac",2027,148765));
+							lapStream.forEach(System.out::println);
+		}
 	
 	public static void main(String[] args) 
 	{
 		emptyStream();
+		createStrm();
+		createStrms();
 	}
 }
