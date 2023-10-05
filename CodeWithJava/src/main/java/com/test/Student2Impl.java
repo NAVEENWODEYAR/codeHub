a700f87fcfbdd69965f78970ead8f35abbeaf870
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 11:38:43 +0530

    Exception in thread "main" java.lang.ClassCastException

--- CodeWithJava/src/main/java/com/test/Student2Impl.java
+++ CodeWithJava/src/main/java/com/test/Student2Impl.java
@@ -8,7 +8,7 @@ public class Student2Impl
 	public static void main(String[] args) 
 	{
 		Set<Student2> studentList = new TreeSet<>();
-					studentList.add(null);
+//					studentList.add(null);
 					studentList.add(new Student2(1,"Kevin","Clara",27,"Abudabi"));
 					studentList.add(new Student2(10,"Jones","Thomas",27,"Berlin"));
 					studentList.add(new Student2(4,"Madam","Dora",87,"Kolar"));
