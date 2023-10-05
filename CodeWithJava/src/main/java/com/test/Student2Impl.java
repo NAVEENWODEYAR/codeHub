d2a45d4571479977ec2d86555478c051864d6cc3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 10:52:41 +0530

    public int compare(Object o1, Object o2)

--- CodeWithJava/src/main/java/com/test/Student2Impl.java
+++ CodeWithJava/src/main/java/com/test/Student2Impl.java
@@ -1,11 +1,19 @@
 package com.test;
 
+import java.util.*;
+
 public class Student2Impl 
 {
 
 	public static void main(String[] args) 
 	{
-		
+		Set<Student2> studentList = new TreeSet(new Comparator() 
+																{
+																	@Override
+																	public int compare(Object o1, Object o2) 
+																	{
+																		return 0;
+																	}});
 	}
 
 }
