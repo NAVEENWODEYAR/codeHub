01645bf23199664aef049ef54c306f089ac0ab25
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 11:00:33 +0530

    Exception in thread "main" java.lang.NullPointerException: Cannot invoke "Object.hashCode()" because "o1" is null

--- CodeWithJava/src/main/java/com/test/Student2Impl.java
+++ CodeWithJava/src/main/java/com/test/Student2Impl.java
@@ -15,6 +15,9 @@ public class Student2Impl
 																		return o1.hashCode() - o2.hashCode();
 																	}});
 					studentList.add(null);
+					studentList.add(new Student2(1,"Kevin","Clara",27,"Abudabi"));
+					studentList.add(new Student2(10,"Jones","Thomas",27,"Berlin"));
+					studentList.add(new Student2(4,"Madam","Dora",87,"Kolar"));
 					System.out.println(studentList);
 	
 	
