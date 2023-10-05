d6d634e076c533d2e6fe794dab4abe1af5d8ce29
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 10:56:15 +0530

    studentList.add(null);

--- CodeWithJava/src/main/java/com/test/Student2Impl.java
+++ CodeWithJava/src/main/java/com/test/Student2Impl.java
@@ -7,13 +7,17 @@ public class Student2Impl
 
 	public static void main(String[] args) 
 	{
-		Set<Student2> studentList = new TreeSet(new Comparator() 
+		Set<Student2> studentList = new TreeSet<Student2>(new Comparator() 
 																{
 																	@Override
 																	public int compare(Object o1, Object o2) 
 																	{
-																		return 0;
+																		return o1.hashCode() - o2.hashCode();
 																	}});
+					studentList.add(null);
+					System.out.println(studentList);
+	
+	
 	}
 
 }
