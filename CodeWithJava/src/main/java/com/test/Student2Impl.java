470b78b62c5f1b6a4f2ac645720509257c528541
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-05 11:41:43 +0530

    [Student2(stId=1, stFirstName=Kevin)

--- CodeWithJava/src/main/java/com/test/Student2Impl.java
+++ CodeWithJava/src/main/java/com/test/Student2Impl.java
@@ -7,12 +7,11 @@ public class Student2Impl
 
 	public static void main(String[] args) 
 	{
-		Set<Student2> studentList = new TreeSet<>();
-//					studentList.add(null);
-					studentList.add(new Student2(1,"Kevin","Clara",27,"Abudabi"));
-					studentList.add(new Student2(10,"Jones","Thomas",27,"Berlin"));
-					studentList.add(new Student2(4,"Madam","Dora",87,"Kolar"));
-					System.out.println(studentList);
+		Set<Student2> studentList2 = new HashSet<>();
+					studentList2.add(new Student2(1,"Kevin","Clara",27,"Abudabi"));
+					studentList2.add(new Student2(10,"Jones","Thomas",27,"Berlin"));
+					studentList2.add(new Student2(4,"Madam","Dora",87,"Kolar"));
+					System.out.println(studentList2);
 	
 	
 	}
