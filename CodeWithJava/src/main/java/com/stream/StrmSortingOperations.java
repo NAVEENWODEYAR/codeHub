9bb78c38af34616a2c73ea483aa8f5abcf4a44a8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-06 22:38:26 +0530

    sorted(Comparator) : Sorting according to supplied comparator

--- CodeWithJava/src/main/java/com/stream/StrmSortingOperations.java
+++ CodeWithJava/src/main/java/com/stream/StrmSortingOperations.java
@@ -1,23 +1,19 @@
 package com.stream;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class StrmSortingOperations 
 {
 	// Sorting operations,
-	// 1. sorted() : Sorting according to natural order
-	static void naturalSorting(Set<Laptop> set)
+	// 1. sorted(Comparator) : Sorting according to supplied comparator
+	static void naturalSorting(List<Laptop> set)
 	{
 //		set.stream().sequential().forEach(System.out::println);
-		set.stream().sorted().skip(2).forEach(System.out::println);
+		set.stream().sorted((l1,l2)->l1.getLapId()-l2.getLapId()).skip(2).forEach(System.out::println);
 	}
 	public static void main(String[] args) 
 	{
-		Set<Laptop> lapSet = new HashSet();
+		List<Laptop> lapSet = new ArrayList();
 					lapSet.add(new Laptop(1,"X1Carbon","Lenovo",2017,198765));
 					lapSet.add(new Laptop(15,"X1Carbon","Lenovo",2017,198765));
 					lapSet.add(new Laptop(7,"Yoga","Lenovo",2019,98765));
