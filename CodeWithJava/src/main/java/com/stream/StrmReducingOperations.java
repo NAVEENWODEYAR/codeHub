857ad2f466ef391f9445e162d5e5405eebb05520
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-06 23:17:25 +0530

    OptionalInt[36]

--- CodeWithJava/src/main/java/com/stream/StrmReducingOperations.java
+++ CodeWithJava/src/main/java/com/stream/StrmReducingOperations.java
@@ -8,8 +8,9 @@ public class StrmReducingOperations
 	// 1. reduce() : Produces a single value
 	static void getSum(int[] nums)
 	{
-		int sum = Arrays.stream(nums).reduce(0,(a,b)->a+b);
-		System.out.println(sum);
+//		int sum = Arrays.stream(nums).reduce(0,(a,b)->a+b);
+		OptionalInt sum = Arrays.stream(nums).reduce((a,b)-> a+b);
+		System.out.println(sum.getAsInt());
 	}
 	public static void main(String[] args) 
 	{
